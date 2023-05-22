78f23ebeb548063fc4100dcb3281074dd6bbaac7
nguyenminh-1992 <nguyenleminh.paris13@gmail.com>
2023-05-22 20:34:03 +0700

    Bai-tap

---
+++ Buoi 1/Bai-tap.py
@@ -0,0 +1,14 @@
+#Tao 1 folder Tinh toan
+#Trong folder Tinh toan tao file coban.py
+#Trong file coban.py tao cac ham tinh cong, tru, nhan, chia
+#Trong folder Tinh toan tao folder Nang cao
+#Trong folder Nang cao tao file nangcao.py
+#Trong file nangcao.py tao cac ham tinh can bac hai, binh phuong.
+
+#Tao 1 file Main.py nam trong cung thu muc voi folder Tinhtoan
+#Tu file Main.py chay cac ham da dinh nghia trong file coban 
+#va nang cao
+
+#Yeu cau o file Main.py nhap tu ban phim.
+
+#Yeu cau them: Them Try-Except
