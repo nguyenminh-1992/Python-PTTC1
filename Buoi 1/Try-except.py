eeae63c98ac75d8006eae607cf0081a0de0f60ad
nguyenminh-1992 <nguyenleminh.paris13@gmail.com>
2023-05-22 19:32:06 +0700

    try - except

---
+++ Buoi 1/Try-except.py
@@ -0,0 +1,20 @@
+def chia(a,b):
+    return a/b
+
+def tinh(a,b):
+    return a+b+2*b+2*a
+
+print("1")
+print("2")
+
+try:
+    print(chia(4,0))
+except:
+    print("Cai nay dang bi loi")
+
+print("3")
+print("4")
+print(tinh(1,2))
+print("5")
+print("6")
+print("7")
