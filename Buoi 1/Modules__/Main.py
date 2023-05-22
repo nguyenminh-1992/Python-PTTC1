48fabfbb8530ac575ec9abe40a81e797826088e8
nguyenminh-1992 <nguyenleminh.paris13@gmail.com>
2023-05-22 20:02:10 +0700

    module

---
+++ Buoi 1/Modules__/Main.py
@@ -0,0 +1,10 @@
+#Cach 1
+#from Tinhtong import hamtinhtong,hamtinhhieu
+
+#Cach 2
+from Tinhtong import *
+a = hamtinhtong(4,5)
+print(a)
+
+b = hamtinhhieu(8,3)
+print(b)
---
+++ Buoi 1/Modules__/Tinhtong.py
@@ -0,0 +1,6 @@
+def hamtinhtong(a,b):
+    return a+b
+
+def hamtinhhieu(a,b):
+    return a-b
+
