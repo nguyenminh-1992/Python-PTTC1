23fefa4388ddd3eb27f25268e35292080ae40b8d
nguyenminh-1992 <nguyenleminh.paris13@gmail.com>
2023-05-24 20:50:30 +0700

    Bai tap quan ly hoc vien

---
+++ Buoi2/Quan_ly_hoc_vien.py
@@ -0,0 +1,9 @@
+#Dinh nghia class PTTC1 gom cac thuoc tinh: ten, tuoi, que quan, 
+# lop, diem tieng anh, diem tin hoc
+#Nhap so luong hoc vien
+#Nhap tay gan cac gia tri cho tung hoc vien 1
+#Show ra danh sach lop PTTC1
+
+#Nang cao: Show theo kieu table
+#Ten   Tuoi   Que quan   Lop   TiengAnh   Tinhoc
+#A      20     HN        1      10         5   
