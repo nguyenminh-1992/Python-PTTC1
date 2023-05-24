dc1964f1226054ed6cc70fcbad144c41ef1b022e
nguyenminh-1992 <nguyenleminh.paris13@gmail.com>
2023-05-24 20:45:42 +0700

    da hinh

--- Buoi2/Class3__.py
+++ Buoi2/Class3__.py
@@ -24,7 +24,16 @@ class PTTC1(BKCAD,Person):
         self.name = name
         self.age = age
     def giaoluu(self):
-        print("Giao luu")
+        print("Giao luu PTTC1")
+
+class PTTC2(BKCAD,Person):
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+    def giaoluu(self):
+        print("Giao luu PTTC2")
+
+
 
 class PTTC1_offline(PTTC1):
     def __init__(self,name,age):
@@ -34,6 +43,15 @@ class PTTC1_offline(PTTC1):
 student1 = PTTC1("Quang", 22)
 student1.hoc()
 student1.an()
+
+student3 = PTTC1("Tien", 32)
+
+#Tinh da hinh
+def da_hinh(student):
+    student.giaoluu()
+
+da_hinh(student1)
+da_hinh(student3)
 
 student2 = PTTC1_offline("Hoang", 23)
 student2.giaoluu()
