b0af85a53ea446115f3d9a82b1da91e1fad67649
nguyenminh-1992 <nguyenleminh.paris13@gmail.com>
2023-05-29 19:50:32 +0700

    main - khung menu

--- Buoi4/Quanlyhocvien/main.py
+++ Buoi4/Quanlyhocvien/main.py
@@ -4,12 +4,14 @@
 #Hien thi
 
 while(True):
-    print("Hello")
-    print("1. Chuc nang them")
-    print("2. Chuc nang xoa")
-    print("3. Chuc nang sua")
-    print("4. Chuc nang hien thi")
-    print("0. Thoat")
+    print("|--------------------------------------|")
+    print("|----Chuong trinh quan ly hoc vien     |")
+    print("|1. Chuc nang them                     |")
+    print("|2. Chuc nang xoa                      |")
+    print("|3. Chuc nang sua                      |")
+    print("|4. Chuc nang hien thi                 |")
+    print("|0. Thoat                              |")
+    print("|--------------------------------------|")
 
     nhap = int(input("Nhap chuc nang theo so: "))
     if nhap==1:
@@ -20,6 +22,9 @@ while(True):
         print("Sua")
     elif nhap==4:
         print("Hien thi")
-    else: 
-        print("Nhap sai ")
+    elif nhap == 0:
+        print("Tam biet")
         break
+    else: 
+        print("Nhap sai vui long nhap lais")
+        
