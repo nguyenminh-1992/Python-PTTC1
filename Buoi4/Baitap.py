831e4c6a5f3cfdefa727f21a52f9facb9af2c845
nguyenminh-1992 <nguyenleminh.paris13@gmail.com>
2023-05-29 20:54:52 +0700

    Bai tap

---
+++ Buoi4/Baitap.py
@@ -0,0 +1,22 @@
+#Chuong trinh quan ly nhan vien
+#Nhan vien(Ten, tuoi, que quan, chuc vu, cham cong (so ngay lam), luong)
+#Chuc vu: TP, NV, GD  -> Show: Truong phong, Nhan vien, Giam doc
+#Cap ma ID, chuc vu + maso
+
+#1 NV1
+#2 TP1
+#3 GD1
+#4 NV2
+#5 NV3
+#6 TP2
+
+#Luong (khong nhap): 300k x chamcong x heso (NV:1 ;  TP: 1,6 ; GD:2)
+#1 ngay lam 300k
+
+#Chuc nang:
+#Them, sua, xoa, hien thi
+
+#Sap xep theo ten ()
+
+#Tim kiem
+
